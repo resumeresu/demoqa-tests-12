55b085b54416afc4d4e83b65c11f2d23cc8a81a5
Svyatoslav Boytsov <sbo@tassta.com>
2022-04-03 01:58:20 +0300

    fillAndSubmitStudentRegistrationForm test added

--- src/test/java/guru/qa/PracticeFormTests.java
+++ src/test/java/guru/qa/PracticeFormTests.java
@@ -3,43 +3,79 @@ package guru.qa;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import java.util.List;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static guru.qa.PracticeFormUtils.*;
+
 
 public class PracticeFormTests {
 
+    //Path
+    String studentRegistrationForm = "/automation-practice-form";
+    //Test data
+    String fistName = "Nikolay";
+    String lastName = "Serpukhov";
+    String email = "nserpukhov@ya.ru";
+    String gender = "Other";
+    String mobile = "1234567890";
+    String birthDay = "28";
+    String birthMonth = "February";
+    String birthYear = "1992";
+    String dateOfBirthFormatted = birthMonth + " " + birthDay + "th, " + birthYear;
+    List<String> subjectsParts = List.of("Che", "Ma", "Ph");
+    List<String> subjectsFull = List.of("Chemistry", "Maths", "Physics");
+    List<String> hobbies = List.of("Reading", "Music");
+    String address = "Street, House Number";
+    String state = "NCR";
+    String city = "Delhi";
+
     @BeforeAll
     static void setUp() {
-        Configuration.holdBrowserOpen = true;
+        //Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
     }
 
-    String studentRegistrationForm = "/automation-practice-form";
-
-    Map<String, String> formData = Map.of(
-            "fistName", "Nikolay",
-            "lastName", "Serpukhov",
-            "email", "nserpukhov@ya.ru",
-            "gender", "Other",
-            "Mobile", "+79998887766",
-            "birthDay", "27",
-            "birthMonth", "February",
-            "birthYear", "1991",
-            "subjects", "Gender studies",
-            "hobbies", "Reading"
-    );
-
     @Test
     void fillAndSubmitStudentRegistrationForm() {
         open(studentRegistrationForm);
-        $("#firstName").setValue(formData.get("fistName"));
-        $("#lastName").setValue(formData.get("lastName"));
-        $("#userEmail").setValue(formData.get("email"));
-    }
 
-}
+        //Filling and submitting the form
+        $("#firstName").setValue(fistName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $(byText(gender)).click();
+        $("#userNumber").setValue(mobile);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(birthMonth);
+        $(".react-datepicker__year-select").selectOption(birthYear);
+        $("[aria-label$='" + dateOfBirthFormatted + "']").click();
+        setSubjectsWithAutocomplete(subjectsParts, subjectsFull);
+        setHobbies(hobbies);
+        $("#uploadPicture").uploadFromClasspath("img/cat.png");
+        $("#currentAddress").setValue(address);
+        $("#state").scrollIntoView(true).click();
+        $(byText(state)).click();
+        $("#city").click();
+        $(byText(city)).click();
+        $("#submit").click();
+
+        //Assertions
+        $("#example-modal-sizes-title-lg").shouldHave(
+                text("Thanks for submitting the form"));
+        $(".modal-body").shouldHave(
+                text(fistName + " " + lastName),
+                text(email),
+                text(gender),
+                text(mobile),
+                text(birthDay + " " + birthMonth + "," + birthYear),
+                text(String.join(", ",subjectsFull)),
+                text("cat.png"),
+                text(address),
+                text(state + " " + city));
+        }
+    }
