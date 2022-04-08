733285e01695da7ea66dad6b63747dd48f595489
Svyatoslav Boytsov <sbo@tassta.com>
2022-04-08 21:02:28 +0300

    fullName format

--- src/test/java/guru/qa/PracticeFormTests.java
+++ src/test/java/guru/qa/PracticeFormTests.java
@@ -10,6 +10,7 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static guru.qa.PracticeFormUtils.*;
+import static java.lang.String.format;
 
 
 public class PracticeFormTests {
@@ -17,21 +18,22 @@ public class PracticeFormTests {
     //Path
     String studentRegistrationForm = "/automation-practice-form";
     //Test data
-    String fistName = "Nikolay";
-    String lastName = "Serpukhov";
-    String email = "nserpukhov@ya.ru";
-    String gender = "Other";
-    String mobile = "1234567890";
-    String birthDay = "28";
-    String birthMonth = "February";
-    String birthYear = "1992";
-    String dateOfBirthFormatted = birthMonth + " " + birthDay + "th, " + birthYear;
+    String fistName = "Nikolay",
+            lastName = "Serpukhov",
+            fullName = format("%s %s", fistName, lastName),
+            email = "nserpukhov@ya.ru",
+            gender = "Other",
+            mobile = "1234567890",
+            birthDay = "28",
+            birthMonth = "February",
+            birthYear = "1992",
+            dateOfBirthFormatted = birthMonth + " " + birthDay + "th, " + birthYear;
     List<String> subjectsParts = List.of("Che", "Ma", "Ph");
     List<String> subjectsFull = List.of("Chemistry", "Maths", "Physics");
     List<String> hobbies = List.of("Reading", "Music");
-    String address = "Street, House Number";
-    String state = "NCR";
-    String city = "Delhi";
+    String address = "Street, House Number",
+            state = "NCR",
+            city = "Delhi";
 
     @BeforeAll
     static void setUp() {
@@ -41,8 +43,8 @@ public class PracticeFormTests {
 
     @Test
     void fillSubmitCloseRegistrationForm() {
-        open(studentRegistrationForm);
 
+        open(studentRegistrationForm);
         executeJavaScript("$('footer').remove()");
         executeJavaScript("$('#fixedban').remove()");
 
@@ -56,6 +58,7 @@ public class PracticeFormTests {
         $(".react-datepicker__month-select").selectOption(birthMonth);
         $(".react-datepicker__year-select").selectOption(birthYear);
         $("[aria-label$='" + dateOfBirthFormatted + "']").click();
+        //$(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
         setSubjectsWithAutocomplete(subjectsParts, subjectsFull);
         setHobbies(hobbies);
         $("#uploadPicture").uploadFromClasspath("img/cat.png");
@@ -68,7 +71,7 @@ public class PracticeFormTests {
 
         //Asserting the data is present
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $x(getValueCell("Student Name")).shouldHave(text(fistName + " " + lastName));
+        $x(getValueCell("Student Name")).shouldHave(text(fullName));
         $x(getValueCell("Student Email")).shouldHave(text(email));
         $x(getValueCell("Gender")).shouldHave(text(gender));
         $x(getValueCell("Mobile")).shouldHave(text(mobile));
